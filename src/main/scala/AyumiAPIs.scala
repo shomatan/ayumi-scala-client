47bfa939ca43cbfc8fb67fa5257a9fdf8919a87b
Shoma Nishitateno <shoma416@gmail.com>
2017-09-01 19:21:31 +0900

    Add tag

--- src/main/scala/AyumiAPIs.scala
+++ src/main/scala/AyumiAPIs.scala
@@ -1,15 +1,23 @@
 package me.shoma.ayumi.client
 
-import me.shoma.ayumi.model.{Category, Post}
+import me.shoma.ayumi.model.{Category, Post, Tag}
 import me.shoma.ayumi.client.Decoders._
 
-class AyumiPosts(endpoint: String) {
+trait AyumiAPI {
+  val client = new HttpClient
+}
+
+class AyumiPosts(endpoint: String) extends AyumiAPI {
 
   def listPosts() = ???
 }
 
+class AyumiCategories(endpoint: String) extends AyumiAPI {
 
-class AyumiCategories(endpoint: String) {
-  val client = new HttpClient
   def listCategories() = client.get[List[Category]](s"$endpoint/categories")
+}
+
+class AyumiTags(endpoint: String) extends AyumiAPI {
+
+  def listTags() = client.get[List[Tag]](s"$endpoint/tags")
 }
--- src/main/scala/Decoders.scala
+++ src/main/scala/Decoders.scala
@@ -2,7 +2,7 @@ package me.shoma.ayumi.client
 
 import io.circe._
 import io.circe.generic.auto._
-import me.shoma.ayumi.model.Category
+import me.shoma.ayumi.model.{Category, Tag}
 
 object Decoders {
 
@@ -11,5 +11,12 @@ object Decoders {
       id <- c.downField("id").as[Option[Long]]
       name <- c.downField("name").as[String]
     } yield Category(id = id, name = name)
+  }
+
+  implicit val decodeTag: Decoder[Tag] = Decoder.instance { c =>
+    for {
+      id <- c.downField("id").as[Option[Long]]
+      name <- c.downField("name").as[String]
+    } yield Tag(id = id, name = name)
   }
 }
