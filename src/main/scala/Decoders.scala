93a1959ac9d79b780c8191597442f759a8af7dd6
Shoma Nishitateno <shoma416@gmail.com>
2017-09-01 23:56:33 +0900

    Custom field decoder

--- src/main/scala/Decoders.scala
+++ src/main/scala/Decoders.scala
@@ -2,7 +2,7 @@ package me.shoma.ayumi.client
 
 import io.circe._
 import io.circe.generic.auto._
-import me.shoma.ayumi.model.{Category, Tag}
+import me.shoma.ayumi.model.{Category, CustomField, Tag}
 
 object Decoders {
 
@@ -18,5 +18,13 @@ object Decoders {
       id <- c.downField("id").as[Option[Long]]
       name <- c.downField("name").as[String]
     } yield Tag(id = id, name = name)
+  }
+
+  implicit val decodeCustomField: Decoder[CustomField] = Decoder.instance { c =>
+    for {
+      postId <- c.downField("postId").as[Long]
+      key <- c.downField("key").as[String]
+      value <- c.downField("value").as[Any]
+    } yield CustomField(postId = postId, key = key, value = value)
   }
 }
